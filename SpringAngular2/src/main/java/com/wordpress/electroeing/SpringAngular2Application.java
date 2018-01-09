93a350b1da39dfe3604c648ac2225bb4dc8dc27a
scardenas <sancardenasv@gmail.com>
2018-01-09 17:56:23 -0500

    Initial Commit

---
+++ SpringAngular2/src/main/java/com/wordpress/electroeing/SpringAngular2Application.java
@@ -0,0 +1,12 @@
+package com.wordpress.electroeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngular2Application.class, args);
+	}
+}
